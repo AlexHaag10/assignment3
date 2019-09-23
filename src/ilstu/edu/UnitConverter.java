30997234584a3f76d56e8c31d6e513a25421e6c0
Alex <alexhaag10@gmail.com>
2019-09-23 11:54:30 -0500

    Empty Frame

---
+++ src/ilstu/edu/UnitConverter.java
@@ -0,0 +1,31 @@
+package ilstu.edu;
+	
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+
+public class UnitConverter extends JFrame{
+	
+		
+		public UnitConverter() {
+			
+			JFrame frame = new JFrame("Unit Converter");
+			frame.setSize(600, 600);
+			JPanel pane1 = new JPanel();
+			frame.add(pane1);
+			frame.setVisible(true);
+			
+		}
+	
+	public static void main(String[] args) {
+		
+		UnitConverter mf = new UnitConverter();
+		
+		
+		
+		
+		
+	}
+
+}
