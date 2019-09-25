5fdaaa37c61c0c363355681d0107e8470f8c5ba8
Alex <alexhaag10@gmail.com>
2019-09-24 19:35:23 -0500

    Added some example classes

--- src/ilstu/edu/TabbedPaneDemo.java
+++ src/ilstu/edu/TabbedPaneDemo.java
@@ -21,12 +21,12 @@ public class TabbedPaneDemo extends JPanel {
         ImageIcon icon = createImageIcon("images/middle.gif");
         
         JComponent panel1 = makeTextPanel("Panel #1");
-        tabbedPane.addTab("Tab 1", icon, panel1,
+        tabbedPane.addTab("Distance", icon, panel1,
                 "Does nothing");
         tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
         
         JComponent panel2 = makeTextPanel("Panel #2");
-        tabbedPane.addTab("Tab 2", icon, panel2,
+        tabbedPane.addTab("Weight", icon, panel2,
                 "Does twice as much nothing");
         tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
         
@@ -35,8 +35,7 @@ public class TabbedPaneDemo extends JPanel {
                 "Still does nothing");
         tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
         
-        JComponent panel4 = makeTextPanel(
-                "Panel #4 (has a preferred size of 410 x 50).");
+        JComponent panel4 = makeTextPanel("Panel #4 (has a preferred size of 410 x 50).");
         panel4.setPreferredSize(new Dimension(410, 50));
         tabbedPane.addTab("Tab 4", icon, panel4,
                 "Does nothing at all");
@@ -48,7 +47,7 @@ public class TabbedPaneDemo extends JPanel {
         //The following line enables to use scrolling tabs.
         tabbedPane.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
     }
-    
+   //----------------------makeTextPanel------------------------------
     protected JComponent makeTextPanel(String text) {
         JPanel panel = new JPanel(false);
         JLabel filler = new JLabel(text);
